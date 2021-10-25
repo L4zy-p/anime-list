0f9a073be148ce66efa548df32cb8a971df7a9b3
pacharamai <pacharamai@hospital-os.com>
2021-10-25 18:32:24 +0700

    [fix] next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   reactStrictMode: true,
   images: {
+    loader: "imgix",
+    path: "https://kitsu.io/",
     domains: ['media.kitsu.io'],
   },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
