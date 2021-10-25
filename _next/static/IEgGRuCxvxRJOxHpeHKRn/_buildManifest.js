0a2702f4f871788a7e5d19cc57ccc15b3ceb8837
L4zy-p <92322801+L4zy-p@users.noreply.github.com>
2021-10-25 11:46:10 +0000

    Deploying to gh-pages from  @ 53d6e76a7ae22da78f8097ecee12374a140f5bf8 🚀

---
+++ _next/static/IEgGRuCxvxRJOxHpeHKRn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/25-f991e02116c7177596eb.js","static/chunks/pages/index-8a60df4b2ea1fbe41b12.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
