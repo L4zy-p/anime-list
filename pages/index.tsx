19fc5cc71120c420f86c47a2f6d553b43ca19b50
pacharamai <pacharamai@hospital-os.com>
2021-10-25 18:14:56 +0700

    [page]: image background

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,32 +31,42 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Anime List</title>
-        <meta name="description" content='anime list next app' />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='anime list next app' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
       <div className='first-section'>
-        <Row className='nav-header' gutter={[15, 15]} align='middle'>
-          <Col className='web-name' flex='auto'>MyAnimeList</Col>
-          <Col>Anime</Col>
-          <Col>Manga</Col>
-          <Col>Community</Col>
-          <Col>Industry</Col>
-          <Col>Watch</Col>
-          <Col>Read</Col>
-          <Col>Help</Col>
-          <Col><Button shape='circle' icon={<SearchOutlined />} /></Col>
-          <Col><Avatar>P</Avatar></Col>
-        </Row>
-        <Carousel />
+        <div className='demo-wrap'>
+          <img
+            className='demo-bg'
+            src='/ani-background.jpg'
+            alt=''
+          />
+          <div className='demo-content'>
+            <Row className='nav-header' gutter={[15, 15]} align='middle'>
+              <Col className='web-name' flex='auto'>MyAnimeList</Col>
+              <Col>Anime</Col>
+              <Col>Manga</Col>
+              <Col>Community</Col>
+              <Col>Industry</Col>
+              <Col>Watch</Col>
+              <Col>Read</Col>
+              <Col>Help</Col>
+              <Col><Button shape='circle' icon={<SearchOutlined />} /></Col>
+              <Col><Avatar>P</Avatar></Col>
+            </Row>
+            <Carousel />
+          </div>
+        </div>
       </div>
       <div className='second-section'>
         <Row>
           <Col span={24}>
             <h1>
-              Welcome to <a href="https://nextjs.org">Next.js!</a>
+              Highest Rated Anime
             </h1>
           </Col>
         </Row>
+        <br />
         <AnimeList />
       </div>
     </>
